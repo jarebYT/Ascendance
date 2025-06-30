09eb9394f39a71675e68aa2c67188e013ef70fef
jarebYT <jarebYT@hotmail.com>
2025-06-30 14:50:25 +0200

    [~] main: Modified the tilegrid and cam script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,26 +9,25 @@ public class CameraFollow : MonoBehaviour
     public Collider2D wallLeft;
     public Collider2D wallRight;
 
-    private float minX;
-    private float maxX;
+    private float leftLimit;
+    private float rightLimit;
     private float camHalfWidth;
 
     void Start()
     {
-        // Calcul de la moitié de la largeur de la caméra (en unités monde)
         float camHeight = Camera.main.orthographicSize * 2f;
         float camWidth = camHeight * Camera.main.aspect;
         camHalfWidth = camWidth / 2f;
 
-        // Déduction des limites en fonction des murs
-        minX = wallLeft.bounds.max.x + camHalfWidth;
-        maxX = wallRight.bounds.min.x - camHalfWidth;
+        // Déduction des limites en fonction des murs gauche et droite
+        leftLimit = wallLeft.bounds.max.x + camHalfWidth;
+        rightLimit = wallRight.bounds.min.x - camHalfWidth;
     }
 
     void LateUpdate()
     {
         float targetX = player.position.x + offset.x;
-        targetX = Mathf.Clamp(targetX, minX, maxX);
+        targetX = Mathf.Clamp(targetX, leftLimit, rightLimit);
 
         transform.position = new Vector3(targetX, transform.position.y, offset.z);
     }
