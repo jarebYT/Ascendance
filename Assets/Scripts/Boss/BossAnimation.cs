1151dacbe1e312e598f3d866074bfa932c445e5c
jarebYT <jarebYT@hotmail.com>
2025-07-04 14:15:18 +0200

    [+] main: Changed the waking up animation + Boss animation

--- Assets/Scripts/Boss/BossAnimation.cs
+++ Assets/Scripts/Boss/BossAnimation.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class BossAnimation : MonoBehaviour
 {
+
+    private Animator animator;
+    private BossScript bossScript;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Walk()
+    {
+        animator.SetBool("isWalking", true);
+    }
+
+    public void StopWalk()
+    {
+        animator.SetBool("isWalking", false);
     }
 }
