efc8c2b8f3b3e8c0be783c7630fc9a6e1afb4da8
Raphaël Bedleem <rbedleem@myges.fr>
2025-07-05 15:35:53 +0200

    [+] fix player die

---
+++ Assets/Scripts/Player/HealthManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    private PlayerMovement playerMovement;
+
+    public HealthBar healthBar;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetCurrentHealth(currentHealth);
+        Debug.Log($"Joueur prend {damage} dégâts. Santé restante: {currentHealth}");
+        if (currentHealth <= 0)
+        {
+            StartCoroutine(Die());
+        }
+    }
+
+    IEnumerator Die()
+    {
+        playerMovement.canMove = false; // Désactiver le mouvement du joueur
+                                        // Désactiver tous les scripts sauf HealthManager
+        foreach (MonoBehaviour script in GetComponents<MonoBehaviour>())
+        {
+            if (script != this)
+                script.enabled = false;
+        }
+        GameObject boss = GameObject.FindWithTag("enemy");
+        if (boss != null)
+        {
+            foreach (MonoBehaviour script in boss.GetComponents<MonoBehaviour>())
+            {
+                script.enabled = false;
+            }
+        }
+        yield return new WaitForSeconds(5f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("mainMenu");
+    }
+}
+ 
